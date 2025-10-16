5cbf2aef441d004fb97b508372c8fa80312ba1a1
developeravik <avikd906@proton.me>
2025-10-16 23:31:52 +0530

    problem 3 solved

---
+++ src/problem3.ts
@@ -0,0 +1,7 @@
+function concatenateArrays<T>(...arrays: T[][]): T[] {
+  return arrays.flat();
+}
+
+console.log(concatenateArrays(["a", "b"], ["c"]));       
+console.log(concatenateArrays([1, 2], [3, 4], [5]));     
+console.log(concatenateArrays([true, false], [false])); 
